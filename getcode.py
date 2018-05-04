30b93c0ef64b835c10ee2e88f1f4f3dab3aed5b6
broadsun <onlyforsgb@163.com>
2018-05-04 20:08:55 +0800

    update get code

--- getcode.py
+++ getcode.py
@@ -1,17 +1,19 @@
 import requests
-import re
 import sys
-outdir=sys.argv[1]
 
-
-url=sys.argv[2]
-r = requests.get(url)
-c = r.content
-
-l=c.split("href=\"")
-for le in l:
-    if le.find(".c") != -1 and le.find(".c.html") == -1:
-        sl=le.split("\">")
-        rc= requests.get(url+sl[0])
-        with open(outdir+"/"+sl[0], "w") as code:
-           code.write(rc.content)
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print "python getcode.py [url] [outdir]"
+    else:
+        url=sys.argv[1]
+        outdir=sys.argv[2]
+        r = requests.get(url)
+        c = r.content
+        
+        l=c.split("href=\"")
+        for le in l:
+            if le.find(".c") != -1 and le.find(".c.html") == -1:
+                sl=le.split("\">")
+                rc= requests.get(url+sl[0])
+                with open(outdir+"/"+sl[0], "w") as code:
+                    code.write(rc.content)
